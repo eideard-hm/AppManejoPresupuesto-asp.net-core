c03cb72253d9f08be45dc750e36cff1c2e76427c
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-06 08:33:56 -0500

    Se realizo la conexión a SQL Server con Dapper

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,12 +1,26 @@
-﻿using ManejoPresupuesto.Models;
+﻿using Dapper;
+using ManejoPresupuesto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController: Controller
     {
+        private readonly string connectionString;
+
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Crear()
         {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
+
             return View();
         }
 
---
+++ ManejoPresupuestoDB.sql
@@ -0,0 +1,93 @@
+create database ManejoPresupuesto;
+use ManejoPresupuesto;
+
+create table Transacciones
+(
+Id int identity(1,1) primary key not null,
+UsuarioId int not null,
+FechaTransaccion datetime not null,
+Monto decimal(18, 2) not null,
+TipoTransaccionId int not null,
+Nota varchar(1000),
+CuentaId int not null
+);
+
+create table TiposOperaciones
+(
+Id int identity(1,1) primary key not null,
+Descripcion varchar(50) not null
+);
+
+create table TiposCuentas
+(
+Id int identity(1,1) primary key not null,
+Nombre varchar(50) not null,
+UsuarioId int not null,
+Orden int not null
+);
+
+create table Cuentas
+(
+Id int identity(1,1) primary key not null,
+Nombre varchar(50) not null,
+TipoCuentaId int not null,
+Balance decimal(18,2) not null,
+Descripcion varchar(1000),
+);
+
+create table Usuarios
+(
+Id int identity(1,1) primary key,
+Email varchar(100) not null,
+EmailNormalizado varchar(100) not null,
+PasswordHash varchar(200) not null,
+);
+
+create table Categorias
+(
+Id int identity(1,1) primary key not null,
+Nombre varchar(100) not null,
+UsuarioId int not null,
+TipoOperacionId int not null
+);
+
+-- relación entre tablas
+
+ALTER TABLE Transacciones ADD CONSTRAINT FK_TiposOperaciones
+FOREIGN KEY (TipoTransaccionId) REFERENCES TiposOperaciones(Id);
+
+ALTER TABLE Transacciones ADD CONSTRAINT FK_Transacciones_Cuentas
+FOREIGN KEY (CuentaId) REFERENCES Cuentas(Id);
+
+ALTER TABLE Transacciones ADD CONSTRAINT FK_Transacciones_Usuarios
+FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id);
+
+ALTER TABLE Cuentas ADD CONSTRAINT FK_Cuentas_TiposCuentas
+FOREIGN KEY (TipoCuentaId) REFERENCES TiposCuentas(Id);
+
+ALTER TABLE TiposCuentas ADD CONSTRAINT FK_TiposCuentas_Usuarios
+FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id);
+
+ALTER TABLE Categorias ADD CONSTRAINT FK_Categorias_Usuarios
+FOREIGN KEY (UsuarioId) REFERENCES Usuarios(Id);
+
+ALTER TABLE Categorias ADD CONSTRAINT FK_Categorias_TiposOperaciones
+FOREIGN KEY (TipoOperacionId) REFERENCES TiposOperaciones(Id);
+
+-- insertar datos
+INSERT INTO Transacciones(UsuarioId, FechaTransaccion, Monto, TipoTransaccionId, Nota)
+VALUES('Edier', '2021-11-09', 1500.99, 1, 'Esto es una transacción de prueba');
+
+INSERT INTO Transacciones(UsuarioId, FechaTransaccion, Monto, TipoTransaccionId, Nota)
+VALUES('Edier', '2021-11-08', 300, 2, null);
+
+INSERT INTO TiposOperaciones(Descripcion)
+VALUES('Ingresos');
+
+INSERT INTO TiposOperaciones(Descripcion)
+VALUES('Gastos');
+
+-- mostrar los datos
+SELECT * FROM Transacciones;
+
+SELECT * FROM TiposOperaciones;
